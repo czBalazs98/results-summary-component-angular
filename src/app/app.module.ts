0f1c1d67c4431768a72260c6cb1567d82d64371a
bczmarko <bczmarko@intrend.hu>
2023-09-14 15:22:58 +0200

    added result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
