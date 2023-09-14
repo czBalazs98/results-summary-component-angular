a14f8fcc33fc41c6e5dabf92ff92c8bd84fd6f7b
bczmarko <bczmarko@intrend.hu>
2023-09-14 16:17:55 +0200

    result component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ResultComponent } from './components/result/result.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { ResultsSummaryComponent } from './components/results-summary/results-summary.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResultComponent
+    ResultComponent,
+    SummaryComponent,
+    ResultsSummaryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
